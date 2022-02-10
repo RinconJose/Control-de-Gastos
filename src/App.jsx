5782ad616a0562078b764c9191b8e0ff6d617e25
Jose Rincon <rincon.jmg@gmail.com>
2022-02-09 22:09:57 -0500

    creando una función para generar ID único y agregando al state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Modal from './components/Modal';
+import { generarId } from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
+  const [gastos, setGastos] = useState([])
+
   const handleNuevoGasto = () => {
     setModal(true);
 
@@ -21,7 +24,13 @@ function App() {
   }
 
   const guardarGasto = gasto => {
-    console.log(gasto);
+    gasto.id = generarId();
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)
+    }, 500);
   }
 
   return (
