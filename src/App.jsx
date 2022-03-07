cf13bbc66db657f8388780641a0d4ed672b4c4ab
Jose Rincon <rincon.jmg@gmail.com>
2022-03-07 06:49:13 -0500

    Agregar gastos en Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 function App() {
 
   // Componentes que pasan por más de un componente
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
 
   const [presupuesto, setPresupuesto] = useState(
     Number(localStorage.getItem('presupuesto')) ?? 0
@@ -34,6 +36,11 @@ function App() {
   useEffect(() => {
     localStorage.setItem('presupuesto', presupuesto ?? 0)
   },[presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [gastos])
+  
 
   useEffect(() => {
     const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0
