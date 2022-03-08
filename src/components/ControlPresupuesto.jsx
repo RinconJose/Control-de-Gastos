ce6c8e87b88f88c246113bacd20244fa76c8a7be
Jose Rincon <rincon.jmg@gmail.com>
2022-03-08 06:57:21 -0500

    Agregando botón para reiniciar la app y finalizando proyecto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -2,7 +2,13 @@ import { useState, useEffect } from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 
-const ControlPresupuesto = ({gastos, presupuesto}) => {
+const ControlPresupuesto = ({
+    gastos, 
+    setGastos,
+    presupuesto,
+    setPresupuesto,
+    setIsValidPresupuesto
+  }) => {
 
   const [porcentaje, setPorcentaje] = useState(0);
   const [disponible, setDisponible] = useState(0);
@@ -30,6 +36,16 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
     })
   }
 
+  const handleResetApp = () => {
+    const resultado = confirm('¿Deseas reiniciar el presupuesto y el gasto?');
+
+    if(resultado) {
+      setGastos([])
+      setPresupuesto(0)
+      setIsValidPresupuesto(false)
+    }
+  }
+
   return (
       <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
           <div>
@@ -44,6 +60,13 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
             />
           </div>
           <div className='contenido-presupuesto'>
+            <button 
+              className='reset-app'
+              type='button'
+              onClick={handleResetApp}
+            >
+              Resetear app
+            </button>
             <p>
                 <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
             </p>
